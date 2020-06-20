7beb74f4a9c665675e4b5064f2023b532ad05b9a
christopher-hsu <christopherhsu8@gmail.com>
2020-06-20 17:19:22 -0400

    perimeter radius param

--- maPDenv/metadata.py
+++ maPDenv/metadata.py
@@ -65,7 +65,8 @@ METADATA_pd={   #Beliefs are initialized near target
         'const_q': 0.001, # target noise constant in beliefs.
         'const_q_true': 0.01, # target noise constant of actual targets.
         'spiral_min': 0.001, # minimum factor of target spiral
-        'spiral_max': 0.01  # maximum factor of target spiral
+        'spiral_max': 0.01,  # maximum factor of target spiral
+        'perimeter_radius': 10.0 # radius of the perimeter to defend
     }
 
 # Designate a metadata version to be used throughout the target tracking env.
