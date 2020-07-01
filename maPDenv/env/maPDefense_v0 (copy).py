281697e614c0bda6a121179b3aa51c9978206741
christopher-hsu <christopherhsu8@gmail.com>
2020-07-01 10:30:29 -0400

    recopy good pd-v0

--- maPDenv/env/maPDefense_v0 (copy).py
+++ maPDenv/env/maPDefense_v0 (copy).py
@@ -8,7 +8,7 @@ from maPDenv.agent_models import *
 from maPDenv.belief_tracker import * #KFbelief
 from maPDenv.metadata import METADATA
 from maPDenv.policies import *
-from maPDenv.env.maTracking_Base import maTrackingBase
+from maPDenv.env.maPDefense_Base import maPDefenseBase
 
 """
 Perimeter Defense Environments for Reinforcement Learning.
@@ -23,7 +23,8 @@ o_d : linear distance to the closet obstacle point
 o_alpha : angular distance to the closet obstacle point
 
 [Environment Description]
-Varying number of agents, varying number of randomly moving targets
+Varying number of agents, varying number of sprialing targets
+Defend the perimeter!
 No obstacles
 
 maPDefenseEnv0 : SE2 Target model with UKF belief tracker
@@ -36,7 +37,7 @@ maPDefenseEnv0 : SE2 Target model with UKF belief tracker
 
 """
 
-class maPDefenseEnv0(maTrackingBase):
+class maPDefenseEnv0(maPDefenseBase):
 
     def __init__(self, num_agents=1, num_targets=2, map_name='empty', 
                         is_training=True, known_noise=True, **kwargs):
@@ -48,7 +49,8 @@ class maPDefenseEnv0(maTrackingBase):
         self.nb_targets = num_targets #only for init, will change with reset()
         self.agent_dim = 3
         self.target_dim = 3
-        self.target_init_vel = METADATA['target_init_vel']*np.ones((2,))
+        self.target_init_vel = np.array(METADATA['target_init_vel'])
+        self.perimeter_radius = METADATA['perimeter_radius']
         # LIMIT
         self.limit = {} # 0: low, 1:highs
         self.limit['agent'] = [np.concatenate((self.MAP.mapmin,[-np.pi])), np.concatenate((self.MAP.mapmax, [np.pi]))]
@@ -71,7 +73,7 @@ class maPDefenseEnv0(maTrackingBase):
         self.setup_targets()
         self.setup_belief_targets()
         # Use custom reward
-        self.get_reward()
+        # self.get_reward()
 
     def setup_agents(self):
         self.agents = [AgentSE2(agent_id = 'agent-' + str(i), 
@@ -85,16 +87,11 @@ class maPDefenseEnv0(maTrackingBase):
                         dim=self.target_dim, sampling_period=self.sampling_period, 
                         limit=self.limit['target'],
                         collision_func=lambda x: map_utils.is_collision(self.MAP, x),
-                        policy=SinePolicy(0.1, 0.5, 5.0, self.sampling_period))
+                        policy=SpiralPolicy(self.sampling_period, self.MAP.origin, 
+                                            METADATA['spiral_min'], METADATA['spiral_max']))
                         for i in range(self.num_targets)]
 
     def setup_belief_targets(self):
-        # self.belief_targets = [KFbelief(agent_id = 'target-' + str(i),
-        #                 dim=self.target_dim, limit=self.limit['target'],# A=self.targetA,
-        #                 W=self.target_noise_cov, 
-        #                 obs_noise_func=self.observation_noise, 
-        #                 collision_func=lambda x: map_utils.is_collision(self.MAP, x))
-        #                 for i in range(self.num_targets)]
         self.belief_targets = [UKFbelief(agent_id = 'target-' + str(i),
                         dim=self.target_dim, limit=self.limit['target'], 
                         fx=SE2Dynamics,
@@ -105,7 +102,29 @@ class maPDefenseEnv0(maTrackingBase):
 
 
     def get_reward(self, obstacles_pt=None, observed=None, is_training=True):
-        return reward_fun(self.nb_targets, self.belief_targets, is_training)
+        return self.reward_fun(observed, self.origin_init_pos, 
+                            self.perimeter_radius, is_training)
+
+    def reward_fun(self, observed, goal_origin, goal_radius, 
+                    is_training=True, c_mean=0.1):
+        """ Return a reward for targets that enter the goal radius or observed
+        +1 for observed, -1 for entering goal radius
+        """
+        intruder = observed.astype(float)
+        target_states = [target.state for target in self.targets[:self.nb_targets]]
+        global_states = util.global_relative_measure(target_states, goal_origin)
+        intruder[global_states[:,0] < goal_radius] = -1
+
+        reward = np.sum(intruder)
+        done = False
+        mean_nlogdetcov = 0.0
+
+        #if captured or entered goal reset target pose
+        for ii, rew in enumerate(intruder):
+            if rew != 0:
+                self.reset_target_pose(target_id=ii)
+
+        return reward, done, mean_nlogdetcov
 
     def reset(self,**kwargs):
         """
@@ -121,14 +140,13 @@ class maPDefenseEnv0(maTrackingBase):
             self.nb_targets = np.random.random_integers(1, self.num_targets)
         obs_dict = {}
         init_pose = self.get_init_pose(**kwargs)
-        # Initialize all agents
-        for ii in range(self.num_agents):
+        # Initialize agents
+        for ii in range(self.nb_agents):
             self.agents[ii].reset(init_pose['agents'][ii])
-            # Only for nb agents in this episode
-            if ii < self.nb_agents:
-                obs_dict[self.agents[ii].agent_id] = []
-        # Initialize all targets and beliefs
-        for nn in range(self.num_targets):
+            obs_dict[self.agents[ii].agent_id] = []
+
+        # Initialize targets and beliefs
+        for nn in range(self.nb_targets):
             self.belief_targets[nn].reset(
                         init_state=init_pose['belief_targets'][nn],
                         init_cov=self.target_init_cov)
@@ -151,6 +169,7 @@ class maPDefenseEnv0(maTrackingBase):
         reward_dict = {}
         done_dict = {'__all__':False}
         info_dict = {}
+        all_observations = np.zeros(self.nb_targets, dtype=bool)
 
         # Targets move (t -> t+1)
         for n in range(self.nb_targets):
@@ -163,51 +182,65 @@ class maPDefenseEnv0(maTrackingBase):
 
             action_vw = self.action_map[action_dict[agent_id]]
 
-            # Locations of all targets and agents in order to maintain a margin between them
+            # Locations of targets and agents in order to maintain a margin between them
             margin_pos = [t.state[:2] for t in self.targets[:self.nb_targets]]
             for p, ids in enumerate(action_dict):
                 if agent_id != ids:
                     margin_pos.append(np.array(self.agents[p].state[:2]))
             _ = self.agents[ii].update(action_vw, margin_pos)
             
-            observed = []
-            # Update beliefs of all targets
-            for jj in range(self.num_targets):
+            # Target and map observations
+            observed = np.zeros(self.nb_targets, dtype=bool)
+            # obstacles_pt = map_utils.get_closest_obstacle(self.MAP, self.agents[ii].state)
+            # if obstacles_pt is None:
+            obstacles_pt = (self.sensor_r, np.pi)
+
+            # Update beliefs of targets
+            for jj in range(self.nb_targets):
                 # Observe
                 obs = self.observation(self.targets[jj], self.agents[ii])
-                observed.append(obs[0])
-                # self.belief_targets[jj].predict() # Belief state at t+1
-                # if obs[0]: # if observed, update the target belief.
-                #     self.belief_targets[jj].update(obs[1], self.agents[ii].state)
+                observed[jj] = obs[0]
                 self.belief_targets[jj].update(obs[0], obs[1], self.agents[ii].state,
                                             np.array([np.random.random(),
                                             np.pi*np.random.random()-0.5*np.pi]))
 
-            # obstacles_pt = map_utils.get_closest_obstacle(self.MAP, self.agents[ii].state)
-
-            # if obstacles_pt is None:
-            obstacles_pt = (self.sensor_r, np.pi)
-            # Calculate beliefs on only assigned targets
-            for kk in range(self.nb_targets):
-                r_b, alpha_b = util.relative_distance_polar(self.belief_targets[kk].state[:2],
+                r_b, alpha_b = util.relative_distance_polar(self.belief_targets[jj].state[:2],
                                         xy_base=self.agents[ii].state[:2], 
                                         theta_base=self.agents[ii].state[-1])
                 obs_dict[agent_id].append([r_b, alpha_b,
-                                        np.log(LA.det(self.belief_targets[kk].cov)), 
-                                        float(observed[kk]), obstacles_pt[0], obstacles_pt[1]])
+                                        np.log(LA.det(self.belief_targets[jj].cov)), 
+                                        float(observed[jj]), obstacles_pt[0], obstacles_pt[1]])
             obs_dict[agent_id] = np.asarray(obs_dict[agent_id])
+            all_observations = np.logical_or(all_observations, observed)
+
         # Get all rewards after all agents and targets move (t -> t+1)
-        reward, done, mean_nlogdetcov = self.get_reward(obstacles_pt, observed, self.is_training)
+        reward, done, mean_nlogdetcov = self.get_reward(obstacles_pt, all_observations, self.is_training)
         reward_dict['__all__'], done_dict['__all__'], info_dict['mean_nlogdetcov'] = reward, done, mean_nlogdetcov
         return obs_dict, reward_dict, done_dict, info_dict
 
-def reward_fun(nb_targets, belief_targets, is_training=True, c_mean=0.1):
-    detcov = [LA.det(b_target.cov) for b_target in belief_targets[:nb_targets]]
-    r_detcov_mean = - np.mean(np.log(detcov))# - np.std(np.log(detcov))
-    reward = c_mean * r_detcov_mean
+    def reset_target_pose(self, target_id,
+                lin_dist_range_target=(METADATA['target_init_dist_min'], METADATA['target_init_dist_max']),
+                ang_dist_range_target=(-np.pi, np.pi),
+                lin_dist_range_belief=(METADATA['init_belief_distance_min'], METADATA['init_belief_distance_max']),
+                ang_dist_range_belief=(-np.pi, np.pi),
+                blocked=False):
+        """if captured or entered goal reset target pose and belief
+        """
+        is_target_valid = False
+        while(not is_target_valid):
+            is_target_valid, init_pose_target = self.gen_rand_pose(
+                self.origin_init_pos[:2], self.origin_init_pos[2],
+                lin_dist_range_target[0], lin_dist_range_target[1],
+                ang_dist_range_target[0], ang_dist_range_target[1])
 
-    mean_nlogdetcov = None
-    if not(is_training):
-        logdetcov = [np.log(LA.det(b_target.cov)) for b_target in belief_targets[:nb_targets]]
-        mean_nlogdetcov = -np.mean(logdetcov)
-    return reward, False, mean_nlogdetcov
+        is_belief_valid, init_pose_belief = False, np.zeros((2,))
+        while((not is_belief_valid) and is_target_valid):
+            is_belief_valid, init_pose_belief = self.gen_rand_pose(
+                init_pose_target[:2], init_pose_target[2],
+                lin_dist_range_belief[0], lin_dist_range_belief[1],
+                ang_dist_range_belief[0], ang_dist_range_belief[1])
+
+        self.belief_targets[target_id].reset(
+                    init_state=init_pose_belief,
+                    init_cov=self.target_init_cov)
+        self.targets[target_id].reset(init_pose_target)
