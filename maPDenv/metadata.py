d3cf92b3a13710c1934f640d8c7496bc817740b1
christopher-hsu <christopherhsu8@gmail.com>
2020-07-10 10:14:28 -0400

    adjusted action primitives and sensor ranges

--- maPDenv/metadata.py
+++ maPDenv/metadata.py
@@ -71,8 +71,8 @@ METADATA_pd_v0={   #Beliefs are initialized near target
 
 METADATA_pd_v1={   #Beliefs are initialized near target
         'version' : 1,
-        'sensor_r': 10.0,   # sensor range capture
-        'sensor_r_long': 30.0,   # long range sensor for scouting
+        'sensor_r': 7.0,   # sensor range capture
+        'sensor_r_long': 20.0,   # long range sensor for scouting
         'fov' : 90,
         'sensor_r_sd': 0.2, # sensor range noise.
         'sensor_b_sd': 0.01, # sensor bearing noise.
@@ -80,15 +80,15 @@ METADATA_pd_v1={   #Beliefs are initialized near target
         'agent_init_dist_max': 15.0, # the maximum distance btw agent and the origin.
         'target_init_cov': 30.0, # initial target diagonal Covariance.
         'target_init_vel': [0.0, 0.0], # target's initial velocity.
-        'target_vel_limit': 2.0, # velocity limit of targets.
-        'target_init_dist_min': 40.0, # the minimum distance btw targets and the origin.
-        'target_init_dist_max': 40.0, # the maximum distance btw targets and the origin.
+        'target_vel_limit': 3.0, # velocity limit of targets.
+        'target_init_dist_min': 45.0, # the minimum distance btw targets and the origin.
+        'target_init_dist_max': 45.0, # the maximum distance btw targets and the origin.
         'init_belief_distance_min': 0.0, # the minimum distance btw belief and the target.
         'init_belief_distance_max': 5.0, # the maximum distance btw belief and the target.
         'margin': 1.0, # a marginal distance btw targets and the agent.
         'margin2wall': 0.5, # a marginal distance from a wall.
-        'action_v': [2, 1.33, 0.67, 0], # action primitives - linear velocities.
-        'action_w': [np.pi/2, 0, -np.pi/2], # action primitives - angular velocities.
+        'action_v': [3, 2, 1, 0], # action primitives - linear velocities.
+        'action_w': [np.pi/2, np.pi/4, 0, -np.pi/4, -np.pi/2], # action primitives - angular velocities.
         'const_q': 0.001, # target noise constant in beliefs.
         'const_q_true': 0.01, # target noise constant of actual targets.
         'spiral_min': 0.05, # minimum factor of target spiral
