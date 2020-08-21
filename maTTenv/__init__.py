30f83a49ba545c042e8251f14c401058e23ad6b4
christopher-hsu <christopherhsu8@gmail.com>
2020-08-21 09:32:25 -0400

    special env for testing behavior

--- maTTenv/__init__.py
+++ maTTenv/__init__.py
@@ -40,6 +40,9 @@ def make(env_name, render=False, figID=0, record=False, ros=False, directory='',
     elif env_name == 'setTracking-v1':
         from maTTenv.env.setTracking_v1 import setTrackingEnv1
         env0 = setTrackingEnv1(num_agents=num_agents, num_targets=num_targets, **kwargs)
+    elif env_name == 'setTracking-behavior':
+        from maTTenv.env.setTracking_behavior import setTrackingEnvBehavior
+        env0 = setTrackingEnvBehavior(num_agents=num_agents, num_targets=num_targets, **kwargs)
 
     elif env_name == 'advTracking-v0':
         from maTTenv.env.advTracking_v0 import advTrackingEnv0
